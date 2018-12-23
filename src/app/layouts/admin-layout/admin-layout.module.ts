c841582a6cdf7108917d961f1b845d232de8fcc3
ToanHD <toanhanduc@gmail.com>
2018-12-24 01:08:59 +0700

    rm unused components

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -3,32 +3,17 @@ import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AdminLayoutRoutes} from './admin-layout.routing';
-import {DashboardComponent} from '../../dashboard/dashboard.component';
-import {UserProfileComponent} from '../../user-profile/user-profile.component';
-import {TableListComponent} from '../../table-list/table-list.component';
-import {TypographyComponent} from '../../typography/typography.component';
-import {IconsComponent} from '../../icons/icons.component';
-import {MapsComponent} from '../../maps/maps.component';
-import {NotificationsComponent} from '../../notifications/notifications.component';
-import {UpgradeComponent} from '../../upgrade/upgrade.component';
 import {ManageStudentComponent} from '../../manage-student/manage-student.component';
 import {ManageStudentDialogComponent} from '../../manage-student/manage-student-dialog'
 import {ManageLecturerComponent} from '../../manage-lecturer/manage-lecturer.component';
 import {ManageLecturerDialogComponent} from '../../manage-lecturer/manage-lecturer-dialog'
-import {GradingComponent} from '../../grading/grading.component';
-import {TranscriptComponent} from '../../transcript/transcript.component';
-import {TranscriptDialogComponent} from '../../transcript/transcript-dialog'
-import {ManageRequestComponent} from '../../manage-request/manage-request.component'
-import {RequestDialogComponent} from '../../manage-request/request-dialog';
 import {StudentService} from '../../student-services/student.service';
 import {LecturerService} from '../../lecturer-services/lecturer-service.service';
-import {GradingService} from '../../grading-services/grading-service.service';
-import {TranscriptService} from '../../transcript-services/transcript-service.service';
 import {HttpModule} from '@angular/http';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {DatePipe} from '@angular/common';
-
+import {AccountLecturerDialogComponent} from '../../manage-lecturer/account-lecturer-dialog';
 
 import {
     MatButtonModule,
@@ -42,8 +27,6 @@ import {
     MatNativeDateModule,
     MatAutocompleteModule
 } from '@angular/material';
-import {RequestService} from '../../request.service';
-import {AccountLecturerDialogComponent} from '../../manage-lecturer/account-lecturer-dialog';
 
 @NgModule({
     imports: [
@@ -66,39 +49,21 @@ import {AccountLecturerDialogComponent} from '../../manage-lecturer/account-lect
         MatAutocompleteModule,
     ],
     declarations: [
-        DashboardComponent,
-        UserProfileComponent,
-        TableListComponent,
-        TypographyComponent,
-        IconsComponent,
-        MapsComponent,
-        NotificationsComponent,
-        UpgradeComponent,
         ManageStudentComponent,
         ManageStudentDialogComponent,
         ManageLecturerComponent,
         ManageLecturerDialogComponent,
-        GradingComponent,
-        TranscriptComponent,
-        TranscriptDialogComponent,
-        ManageRequestComponent,
-        RequestDialogComponent,
         AccountLecturerDialogComponent
     ],
     entryComponents: [
         ManageStudentDialogComponent,
         ManageLecturerDialogComponent,
-        TranscriptDialogComponent,
-        RequestDialogComponent,
         AccountLecturerDialogComponent
     ],
     providers: [
         StudentService,
         LecturerService,
-        GradingService,
-        TranscriptService,
         DatePipe,
-        RequestService
     ]
 })
 
